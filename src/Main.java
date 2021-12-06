f1ccd50d1115f944f1096542d772477bec799fe4
Katriona17 <krk13@mail.ru>
2021-12-06 22:38:05 +0500

    Files changed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        BmiService service = new BmiService ();
+        BmiService service = new BmiService();
         double weight = 53;
         double height = 1.66;
-        double bmi = weight / (height * height);
-        double result = service.calculate(bmi);
+        double result = service.calculate(weight, height);
         System.out.println(result);
     }
 }
